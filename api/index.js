7f8a701f1da3bb4ed0e3d8f625a45e33f076a021
Satwik Kumar <satwikkr393@gmail.com>
2024-11-12 23:38:51 +0530

    added auth routes

--- api/index.js
+++ api/index.js
@@ -2,8 +2,11 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const morgan = require("morgan");
+const authRoute = require("./routes/auth");
 
 dotenv.config({ path: "./.env" });
+app.use(express.json());
 
 const DB = process.env.DATABASE.replace(
   "<PASSWORD>",
@@ -18,6 +21,9 @@ mongoose
   .catch((err) => {
     console.log(err.message);
   });
+app.use(morgan("dev"));
+
+app.use("/api/v1/auth", authRoute);
 
 app.listen(5000, (req, res) => {
   console.log("Backend is running");
