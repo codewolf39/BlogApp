d1849b68d644a803e0b98f466352ce6e3647bb31
Satwik Kumar <satwikkr393@gmail.com>
2024-11-21 20:36:30 +0530

    changes

--- client/src/components/topbar/Topbar.jsx
+++ client/src/components/topbar/Topbar.jsx
@@ -1,15 +1,16 @@
 import "./topbar.css";
 
 const Topbar = () => {
-  return;
-  <div>
-    <div className="topLeft">
-      <i className="topIcon fab fa-facebook-square"></i>
-      <i className="topIcon fab fa-instagram-square"></i>
-      <i className="topIcon fab fa-pinterest-square"></i>
-      <i className="topIcon fab fa-twitter-square"></i>
+  return (
+    <div>
+      <div className="topLeft">
+        <i className="topIcon fab fa-facebook-square"></i>
+        <i className="topIcon fab fa-instagram-square"></i>
+        <i className="topIcon fab fa-pinterest-square"></i>
+        <i className="topIcon fab fa-twitter-square"></i>
+      </div>
     </div>
-  </div>;
+  );
 };
 
 export default Topbar;
